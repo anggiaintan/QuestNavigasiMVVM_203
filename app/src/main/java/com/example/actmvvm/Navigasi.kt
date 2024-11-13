3be23f658c5591f54556d98963a2f02c6a4c46e0
Anggia Intan <intananggia23@gmail.com>
2024-11-13 22:15:03 +0700

    menambahkan navController sebagai parameter di TampilMahasiswaView untuk melakukan navigasi antar layar

--- app/src/main/java/com/example/actmvvm/Navigasi.kt
+++ app/src/main/java/com/example/actmvvm/Navigasi.kt
@@ -47,7 +47,8 @@ fun Navigasi(
             }
             composable(route = Halaman.Data.name) {
                 TampilMahasiswaView(
-                    mhs = uiState
+                    mhs = uiState,
+                    navController = navHost
                 )
             }
         }
