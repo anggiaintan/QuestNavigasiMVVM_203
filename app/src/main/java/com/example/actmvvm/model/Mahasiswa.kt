8b1b5300d49e9450608fff4094dc4c808dce6546
Anggia Intan <intananggia23@gmail.com>
2024-11-13 22:06:49 +0700

    menambahkan val NIM, email, noHp ke dalam data class Mahasiswa

--- app/src/main/java/com/example/actmvvm/model/Mahasiswa.kt
+++ app/src/main/java/com/example/actmvvm/model/Mahasiswa.kt
@@ -2,6 +2,9 @@ package com.example.actmvvm.model
 
 data class Mahasiswa (
     val nama : String ="",
+    val NIM: String ="",
     val gender: String ="",
-    val alamat : String =""
+    val alamat : String ="",
+    val email : String ="",
+    val noHp : String ="",
 )
